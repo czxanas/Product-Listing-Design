b7196de0928b52a9e1de0fd53052c7e42803ffd7
czxanas <anas.essahib1998@gmail.com>
2024-10-19 04:22:21 +0100

    Add a titlte for accecebility.

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<script setup lang='ts'>
+useSeoMeta({
+  title: 'Products Listing - Cold Banana Test',
+  ogTitle: 'Products Listing - Cold Banana Test',
+  description: 'Rebuild design for my future company, created with ❤️',
+  ogDescription: 'Rebuild design for my future company, created with ❤️',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
     <div class="relative">
         <Container>
