2337f2b636c12d64b538a92533e65f83757758ba
czxanas <anas.essahib1998@gmail.com>
2024-10-19 06:14:34 +0100

    Test website finished.

--- components/Footer/Description.vue
+++ components/Footer/Description.vue
@@ -1,9 +1,9 @@
 <template>
     <div ref="footerTitle" class="relative col-span-5 max-lg:mb-52 max-lg:col-span-6 max-sm:col-span-12">
         <div class="flex flex-col gap-6 text-white">
-            <h3 class="text-3xl font-semibold">CHILLED GRAPE</h3>
+            <h3 class="text-3xl font-semibold" style="-webkit-text-stroke-width: .75px; -webkit-text-stroke-color: #000">CHILLED GRAPE</h3>
 
-            <p class="w-full max-w-[375px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
+            <p class="w-full max-w-[375px]" style="-webkit-text-stroke-width: .25px; -webkit-text-stroke-color: #000">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
                 ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</p>
         </div>
     </div>
